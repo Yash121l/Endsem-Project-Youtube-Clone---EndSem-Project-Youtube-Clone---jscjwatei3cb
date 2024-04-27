81765861e640cf6ae2b07ae82b326116da7e071f
Yash121l <98008765+Yash121l@users.noreply.github.com>
2024-04-27 14:17:36 +0530

    Login page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import "./index.css";
+import * as React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthForm from "./Pages/Auth";
+import Home from "./Pages/Home";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AuthForm/>,
+  },
+  {
+    path: "/homepage",
+    element: <Home/>,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
